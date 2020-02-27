5cb353b3c7b53b3ee32518cec6c8acf34f4fafde
Zein <zabseis@gmail.com>
2020-02-27 14:22:14 +0100

    Recursion ex 18.3 done 'page(729)'

--- src/ADS/Recursion/Recursion.java
+++ src/ADS/Recursion/Recursion.java
@@ -1,5 +1,8 @@
 package ADS.Recursion;
 
+import java.math.BigInteger;
+
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Recursion {
@@ -8,19 +11,34 @@ public class Recursion {
 
         System.out.println("Enter a number");
         int x = scanner.nextInt();
-        //System.out.println("Enter how much");
-        //int n = scanner.nextInt();
+//        System.out.println("Enter how much");
+//        int n = scanner.nextInt();
+//
+//        System.out.println(factorial(x));
+//        System.out.println(fibonacci(x));
+//
+//        xMethod(x);
+//        System.out.println(yMethod(x));
+//
+//        System.out.println(n2(x));
+//        System.out.println(nx(x, n));
+//
+//        System.out.println(compute(x));
+//
+//        zMethod(x);
+//        System.out.println("");
+//        lMethod(x);
+//
+//        System.out.println(fact(x));
 
-        //System.out.println(factorial(x));
-        System.out.println(fibonacci(x));
+//        System.out.println("This is iterative "+fibonacciIterative(x));
+//        System.out.println(" ");
+//        System.out.println("This is recursive "+fibonacci(x));
 
-        //xMethod(x);
-        //System.out.println(yMethod(x));
+        System.out.println("Enter a second number");
+        int y = scanner.nextInt();
 
-        //System.out.println(n2(x));
-        //System.out.println(nx(x, n));
-
-        //System.out.println(compute(x));
+        System.out.println(gcd(x,y));
 
     }
 
@@ -36,6 +54,8 @@ public class Recursion {
     }
 
     // 18.3
+    // the next number is the sum of the last two numbers,
+    // the first number is 1 and the second number is 1.
     static int fibonacci (int n) {
         if (n < 2 ) {
             return n;
@@ -86,6 +106,59 @@ public class Recursion {
         } else {
             return n + compute(n-1);
         }
+    }
+
+    // ex 18.7
+
+    public static void zMethod(int n) {
+        if (n > 0) {
+            System.out.print(n + " ");
+            zMethod(n - 1);
+        }
+    }
+
+    // ex 18.7-2
+    public static void lMethod(int n) {
+        if (n > 0) {
+            lMethod(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+
+    // last ex 181
+    public static BigInteger fact (int n) {
+        BigInteger x = BigInteger.valueOf(n);
+        if (n <= 1) {
+            return x;
+        }
+
+        return x.multiply(fact(x.subtract(BigInteger.valueOf(1)).intValue()));
+    }
+
+    // fibonacci iterative
+    // last ex 18.2
+    public static int fibonacciIterative(int n) {
+        if(n <= 1) {
+            return n;
+        }
+        int fib = 1;
+        int prevFib = 1;
+
+        for(int i=2; i<n; i++) {
+            int temp = fib;
+            fib+= prevFib;
+            prevFib = temp;
+        }
+        return fib;
+    }
+
+    // last ex 18.3
+    public static int gcd (int x, int y) {
+        if (x%y == 0) {
+             return y;
+
+        }
+        return gcd (y, x%y);
     }
 
 }
